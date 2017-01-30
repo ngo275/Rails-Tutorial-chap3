f061a8d110ab113aa14cf3c9d0b2ff74d306446c
Shuichi Nagao <ngo38114@gmail.com>
2017-01-30 09:17:32 +0900

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,20 +2,22 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
-  #test "invalid signup information" do
-    #get signup_path
-    #assert_no_difference 'User.count' do
-      #post users_path, params: {
-      #  user: {
-      #    name: "",
-      #    email: "user@invalid",
-      #    password: "foo",
-      #    password_confirmation: "bar"
-      #  }
-      #}
-    #end
-   # assert_template 'users/new'
-  #end
-  #   assert true
-  # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "user@example.com",
+          password: "foo",
+          password_confirmation: "bar"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.FILL_IN
+  end
+     #assert true
+   #end
 end
